d5dc50a727c77708c9677a09193ebc2f423cf58a
Danny Wade <danny.lee.wade@gmail.com>
2023-04-11 16:49:43 +0100

    Initial Commit

---
+++ WorkLog/WorkLogApp.swift
@@ -0,0 +1,17 @@
+//
+//  WorkLogApp.swift
+//  WorkLog
+//
+//  Created by Danny Wade on 11/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct WorkLogApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

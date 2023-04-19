e685fce4ba09232aaece7a2dee860b1a02f8ad9a
dannywadecmc <d.wade@cmcmarkets.com>
2023-04-19 17:26:46 +0100

    Changes to ProfitLossChart

--- WorkLog/Views/Transaction/ProfitLossChart.swift
+++ WorkLog/Views/Transaction/ProfitLossChart.swift
@@ -2,31 +2,17 @@
 //  ProfitLossChart.swift
 //  WorkLog
 //
-//  Created by Danny Wade on 11/04/2023.
+//  Created by Danny Wade on 18/04/2023.
 //
 
 import SwiftUI
 import Charts
 
-// 1. Data model
-struct Revenue: Identifiable {
-    let id = UUID()
-    let revenueType: ProfitLossType
-    let amount: Double
-    
-    enum ProfitLossType: String, Equatable {
-        case income = "Income"
-        case expense = "Expense"
-    }
-}
-
-// 2. Actual revenue data
-let revenueData: [Revenue] = [
-    Revenue(revenueType: .income, amount: 53711.30),
-    Revenue(revenueType: .expense, amount: 20812.50)
-]
-
 struct ProfitLossChart: View {
+    let revenueData = [
+        (revenueType: "Income", data: RevenueData.incomeAmount),
+        (revenueType: "Expense", data: RevenueData.expenseAmount)
+    ]
     
     var incomeGradient: LinearGradient {
         LinearGradient(colors: [Color("brand.blue.one"), Color("brand.blue.two")], startPoint: .leading, endPoint: .trailing)
@@ -35,38 +21,27 @@ struct ProfitLossChart: View {
     var expenseGradient: LinearGradient {
         LinearGradient(colors: [Color("brand.gray.one"), Color("brand.gray.two")], startPoint: .leading, endPoint: .trailing)
     }
-
     
     var body: some View {
-        VStack {
-            Chart(revenueData) { item in
-                BarMark(
-                    x: .value("Amount", item.amount),
-                    y: .value("Revenue Type", item.revenueType.rawValue)
-                )
-                
-                // 1. Add annotations
-                .annotation(position: .trailing) {
-                    Text(String(format: "£%.0f", item.amount))
-                        .foregroundColor(Color.gray)
-                        .font(.system(size: 12, weight: .bold))
-                        .padding()
+            VStack {
+                Chart {
+                    ForEach(revenueData, id: \.revenueType) { element in
+                        ForEach(element.data) {
+                            BarMark(
+                                x: .value("Amount", $0.amount),
+                                y: .value("Type", $0.incomeExpenseType.rawValue))
+                        }
+                        .foregroundStyle(by: .value("Type", element.revenueType))
+                        .cornerRadius(5)
+                    }
+                    
                 }
-                
-//                .foregroundStyle(.linearGradient(
-//                    colors: [barColor1, barColor2],
-//                    startPoint: .leading,
-//                    endPoint: .trailing))
-                .cornerRadius(10)
+                .chartForegroundStyleScale(["Income": incomeGradient, "Expense": expenseGradient])
+                .chartLegend(.hidden)
             }
-//            .chartForegroundStyleScale(["Income": incomeGradient, "Expense": expenseGradient])
-            .chartXAxis(.hidden)
-            
+            .padding()
         }
-        .padding()
-        
     }
-}
 
 struct ProfitLossChart_Previews: PreviewProvider {
     static var previews: some View {
@@ -74,4 +49,23 @@ struct ProfitLossChart_Previews: PreviewProvider {
     }
 }
 
+struct RevenueData: Identifiable {
+    let id = UUID()
+    let incomeExpenseType: IncomeOrExpense
+    let amount: Int
+    
+    enum IncomeOrExpense: String {
+        case income = "Income"
+        case expense = "Expense"
+    }
+}
 
+extension RevenueData {
+    static let incomeAmount: [RevenueData] = [
+        .init(incomeExpenseType: .income, amount: 3200)
+    ]
+    
+    static let expenseAmount: [RevenueData] = [
+        .init(incomeExpenseType: .expense, amount: 1600)
+    ]
+}
